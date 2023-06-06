1addafc160a984e1b1f6c9f40924ec87267f0ce5
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-06 16:27:43 +0300

    Added array objects and Displayed books and Fixed Eslint Linters

--- script.js
+++ script.js
@@ -2,24 +2,7 @@
 const booksDiv = document.querySelector('.books');
 const form = document.querySelector('.form');
 
-// Retrieve books from localStorage or initialize an empty array
 let books = JSON.parse(localStorage.getItem('books')) || [];
-
-// Function to add a new book to the collection
-function addBook(title, author) {
-  const id = Date.now();
-  const book = { title, author, id };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
-
-// Function to remove a book from the collection
-function removeBook(id) {
-  books = books.filter((book) => book.id !== id);
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
 
 // Function to display all books in the collection
 function displayBooks() {
@@ -37,6 +20,22 @@ function displayBooks() {
     `;
     booksDiv.appendChild(bookUnit);
   });
+}
+
+// Function to add a new book to the collection
+function addBook(title, author) {
+  const id = Date.now();
+  const book = { title, author, id };
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  displayBooks();
+}
+
+// Function to remove a book from the collection
+function removeBook(id) {
+  books = books.filter((book) => book.id !== id);
+  localStorage.setItem('books', JSON.stringify(books));
+  displayBooks();
 }
 
 // Add event listener to the form submit event
@@ -57,7 +56,7 @@ form.addEventListener('submit', (event) => {
 // Add event listener to the booksDiv for the removeBook button clicks
 booksDiv.addEventListener('click', (event) => {
   if (event.target.classList.contains('removeBook')) {
-    const bookId = parseInt(event.target.parentElement.id);
+    const bookId = parseInt(event.target.parentElement.id, 10);
     removeBook(bookId);
   }
 });
